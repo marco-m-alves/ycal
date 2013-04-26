9205a27726d5498fdd68b1e5f19c01cba14af4e8
Marco Alves <marco.m.alves@gmail.com>
2013-04-26 22:54:46 +0100

    Refactors in order to make options and rules available to the user

--- ycal.js
+++ ycal.js
@@ -1,178 +1,113 @@
 angular.module('ycal', [] ).
-directive('ycalendar', function ( FullYearTemplate ) {
+directive('ycalendar', function (YcalRenderer) {
   return {
     restrict: 'E',
-    scope: { year: '=', onSelectionChange: '&' },
+    scope: { year: '=', rules: '=',  options: '=', onSelectionChange: '&' },
     template: '<div class="content-box"></div>',
     link: function (scope, element) {
       
-      
       // Style the calendar
       $(element).addClass('yui3-skin-sam');
-      
-      
-      
-      YUI().use('calendar', 'datatype-date', 'datatype-date-math', function(Y) {
 
-        // Set the default value for the year as the current year
-        var currentYear = new Date().getFullYear();
+      // Setup defaults
+      var defaults = {
+        contentBox: $(element).find('.content-box')[0], // the wrapping template's div
+        width: '100%', 
+        showPrevMonth: true,
+        showNextMonth: true,
+        selectionMode: 'multiple-sticky', // enable multiple selection
+        date: new Date(new Date().getFullYear(), 0, 1) // start at current year
+      };
+
+      // Define options as overriding defaults
+      var options = angular.extend(defaults, scope.options || {});
+      
+      
+      // Bootstrap the YUI widget
+      YUI().use('calendar', 'datatype-date', 'datatype-date-math', function (Y) {
         
-     // Switch the calendar main template to the included two pane template
-     Y.CalendarBase.CONTENT_TEMPLATE = FullYearTemplate;
+         // Switch the calendar main template to the included two pane template
+         Y.CalendarBase.CONTENT_TEMPLATE = YcalRenderer.render(options.format || '3x4');
 
-     // Create a new instance of calendar, setting the showing of previous
-     // and next month's dates to true, and the selection mode to multiple
-     // selected dates. Additionally, set the disabledDatesRule to a name of
-     // the rule which, when matched, will force the date to be excluded
-     // from being selected. Also configure the initial date on the calendar
-     // to be July of 2011.
-     var calendar = new Y.Calendar({
-       contentBox: $(element).find('.content-box')[0],
-       width: "100%",
-       showPrevMonth: true,
-       showNextMonth: true,
-       selectionMode: 'multiple-sticky',
-       date: new Date(currentYear, 0, 1)
-     }).render();
+         // Create a new instance of calendar, setting the showing of previous
+         // and next month's dates to true, and the selection mode to multiple
+         // selected dates. Additionally, set the disabledDatesRule to a name of
+         // the rule which, when matched, will force the date to be excluded
+         // from being selected. Also configure the initial date on the calendar
+         // to be July of 2011.
+         var calendar = new Y.Calendar(options).render();
             
-      scope.calendar = calendar;
-        
-      // Make change to year parameter reflected
-      // on the calendar date range
-      scope.$watch('year', function(){
-        var year = scope.year;
-        //console.log('changed year', year);
-        if (year && calendar) {
-          //console.log('setting date', year);
-          calendar.set('date', new Date(year, 0, 1));
-        }
-      }, true);
+          
+        // Make change to year parameter reflected
+        // on the calendar date range
+        scope.$watch('year', function(){
+          var year = scope.year;
+          //console.log('changed year', year);
+          if (year && calendar) {
+            //console.log('setting date', year);
+            calendar.set('date', new Date(year, 0, 1));
+          }
+        }, true);
 
-      // Create a set of rules to match specific dates. In this case,
-      // the "tuesdays_and_fridays" rule will match any Tuesday or Friday,
-      // whereas the "all_weekends" rule will match any Saturday or Sunday.
-       var rules = {
-         "all": {
+        // Create a set of rules to match specific dates. In this case,
+        // the "tuesdays_and_fridays" rule will match any Tuesday or Friday,
+        // whereas the "all_weekends" rule will match any Saturday or Sunday.
+         var rules = {
            "all": {
              "all": {
-               "0,6": "all_weekends"
+               "all": {
+                 "0,6": "all_weekends"
+               }
              }
            }
-         }
-       };
+         };
 
-      // Set the calendar customRenderer, provides the rules defined above,
-      // as well as a filter function. The filter function receives a reference
-      // to the node corresponding to the DOM element of the date that matched
-      // one or more rule, along with the list of rules. Check if one of the
-      // rules is "all_weekends", and if so, apply a custom CSS class to the
-      // node.
-       calendar.set("customRenderer", {
-         rules: rules,
-         filterFunction: function (date, node, rules) {
-           if (Y.Array.indexOf(rules, 'all_weekends') >= 0) {
-             node.addClass("redtext");
+        // Set the calendar customRenderer, provides the rules defined above,
+        // as well as a filter function. The filter function receives a reference
+        // to the node corresponding to the DOM element of the date that matched
+        // one or more rule, along with the list of rules. Check if one of the
+        // rules is "all_weekends", and if so, apply a custom CSS class to the
+        // node.
+         calendar.set("customRenderer", {
+           rules: rules,
+           filterFunction: function (date, node, rules) {
+             if (Y.Array.indexOf(rules, 'all_weekends') >= 0) {
+               node.addClass("redtext");
+             }
            }
-         }
-       });
-
-        // Set a custom header renderer with a callback function,
-        // which receives the current date and outputs a string.
-        // use the Y.Datatype.Date format to format the date, and
-        // the Datatype.Date math to add one month to the current
-        // date, so both months can appear in the header (since 
-        // this is a two-pane calendar).
-         calendar.set("headerRenderer", function (curDate) {
-           var ydate = Y.DataType.Date,
-               output = ydate.format(curDate, {
-               format: "%B %Y"
-             }) + " &mdash; " + ydate.format(ydate.addMonths(curDate, 11), {
-               format: "%B %Y"
-             });
-           return output;
-         }); 
-
-        // When selection changes, output the fired event to the
-        // console. the newSelection attribute in the event facade
-        // will contain the list of currently selected dates (or be
-        // empty if all dates have been deselected).
-                calendar.on("selectionChange", function (ev) {
-                  scope.$apply(function(){
-                    Y.log(ev.newSelection);
-                    if (scope.onSelectionChange) scope.onSelectionChange({ dates: ev.newSelection }); 
-                  });
          });
+
+          // Set a custom header renderer with a callback function,
+          // which receives the current date and outputs a string.
+          // use the Y.Datatype.Date format to format the date, and
+          // the Datatype.Date math to add one month to the current
+          // date, so both months can appear in the header (since 
+          // this is a two-pane calendar).
+           calendar.set("headerRenderer", function (curDate) {
+             var ydate = Y.DataType.Date,
+                 output = ydate.format(curDate, {
+                 format: "%B %Y"
+               }) + " &mdash; " + ydate.format(ydate.addMonths(curDate, 11), {
+                 format: "%B %Y"
+               });
+             return output;
+           }); 
+
+            // When selection changes, output the fired event to the
+            // console. the newSelection attribute in the event facade
+            // will contain the list of currently selected dates (or be
+            // empty if all dates have been deselected).
+            calendar.on('selectionChange', function (ev) {
+              scope.$apply(function(){
+                Y.log(ev.newSelection);
+                if (scope.onSelectionChange) scope.onSelectionChange({ dates: ev.newSelection }); 
+              });
+            });
          
       });
     }
   };
 })
-.service('TemplateRender', function () {
-
-})
-.constant('FullYearTemplate', '<div class="yui3-g {calendar_pane_class}" id="{calendar_id}">' +  
-                 '{header_template}' +
-               '<div class="yui3-u-1-4">'+
-                       '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-               '<div class="yui3-u-1-4">' +
-                     '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                     '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                      '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-               '<div class="yui3-u-1-4">'+
-                       '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-               '<div class="yui3-u-1-4">' +
-                     '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                     '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                      '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-               '<div class="yui3-u-1-4">'+
-                       '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-               '<div class="yui3-u-1-4">' +
-                     '<div class = "{calendar_left_grid_class}">' +
-                    '{calendar_grid_template}' +
-                     '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                      '</div>' +
-               '</div>' +
-                 '<div class="yui3-u-1-4">' +
-                       '<div class = "{calendar_right_grid_class}">' +
-                    '{calendar_grid_template}' +
-                       '</div>' +
-               '</div>' +
-    '</div>')
 .service('YcalRenderer', function (YcalLayouts, YcalBaseTemplates) {
 
   this.render = function ( layout ) {
@@ -182,7 +117,7 @@ directive('ycalendar', function ( FullYearTemplate ) {
         result;
 
     angular.forEach(layoutParts, function (part) {
-      inner += YcalLayouts[part];
+      inner += YcalBaseTemplates[part];
     });
 
     result = container.replace(/\{__grid__\}/, inner);
@@ -192,9 +127,10 @@ directive('ycalendar', function ( FullYearTemplate ) {
 
 })
 .constant('YcalLayouts', {
-  col2: [ 'left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2'],
-  col3: [ 'left3', 'right3', 'left3', 'right3', 'left3', 'right3', 'left3', 'right3'],
-  col4: [ 'left4', 'right4', 'left4', 'right4', 'left4', 'right4' ]
+  // rows x columns
+  '6x2': ['left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2', 'left2', 'right2'],
+  '4x3': ['left3', 'center3', 'right3', 'left3', 'center3', 'right3', 'left3', 'center3', 'right3', 'left3', 'center3', 'right3'],
+  '3x4': ['left4', 'left4', 'right4', 'right4', 'left4', 'left4', 'right4', 'right4', 'left4', 'left4', 'right4', 'right4']
 })
 .constant('YcalBaseTemplates', {
   container: '<div class="yui3-g {calendar_pane_class}" id="{calendar_id}">' +  
@@ -210,11 +146,20 @@ directive('ycalendar', function ( FullYearTemplate ) {
               '{calendar_grid_template}' +
                  '</div>' +
          '</div>',
-  left2: '<div class="yui3-u-1-3">'+
+  left2: '<div class="yui3-u-1-2">'+
                '<div class = "{calendar_left_grid_class}">' +
             '{calendar_grid_template}' +
                '</div>' +
        '</div>',
+  center4: '<div class="yui3-u-1-4">'+
+              '{calendar_grid_template}' +
+            '</div>',
+  center3: '<div class="yui3-u-1-3">'+
+              '{calendar_grid_template}' +
+          '</div>',
+  center2: '<div class="yui3-u-1-2">'+
+            '{calendar_grid_template}' +
+          '</div>',
   right4: '<div class="yui3-u-1-4">' +
                '<div class = "{calendar_right_grid_class}">' +
             '{calendar_grid_template}' +

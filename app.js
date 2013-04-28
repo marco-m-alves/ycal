e5003f26a7595f58a9edbcc99a03da7a8b9b4174
Marco Alves <marco.m.alves@gmail.com>
2013-04-28 22:44:37 +0100

    Adds weekday selector

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.controller('Ctrl', function($scope){
            "all": {
              "all": {
                "all": {
-                 "0,6": "all_weekends"
+                 "0,6": "custom_rule"
                }
              }
            }
          };
 
     $scope.styles = {
-    	'all_weekends': 'redtext'
+    	'custom_rule': 'redtext'
     };
 
 
@@ -24,4 +24,139 @@ app.controller('Ctrl', function($scope){
 	};
 
 
+	$scope.$watch('selectedWeekdays', function () {
+
+		if (!$scope.selectedWeekdays) return;
+
+		var _weekdays = $scope.selectedWeekdays.join(',');
+
+		console.log("0,6", _weekdays);
+
+		var rules = {
+           "all": {
+             "all": {
+               "all": {
+                 // weekdays will be inserted here
+               }
+             }
+           }
+         };
+
+		rules.all.all.all[_weekdays] = "custom_rule";
+
+         $scope.rules = rules;
+	}, true);
+
+});
+
+app.directive('weekdaySelect', function(){
+  return {
+    restrict: 'E',
+    scope: { selectedWeekdays: '=', selectedOrdinals: '=', showOrdinals: '=' },
+    template: '<div ng-show="showOrdinals"><span ng-repeat="ordinal in ordinals" style="margin-right: 10px;"><input class="margin-reset" type="checkbox" ng-model="ordinal.selected" ng-change="updateSelection()">&nbsp;{{ordinal.name}}</span></div><span ng-repeat="weekday in weekdays" style="margin-right: 10px;"><input type="checkbox" ng-model="weekday.selected" ng-change="updateSelection()">&nbsp;{{weekday.name}}</span>',
+    link: function( scope, element) {
+      // var weekdays = [
+      //   { name: 'Dom', value: 'Sunday' },
+      //   { name: 'Seg', value: 'Monday' },
+      //   { name: 'Ter', value: 'Tuesday' },
+      //   { name: 'Qua', value: 'Wednesday' },
+      //   { name: 'Qui', value: 'Thursday' },
+      //   { name: 'Sex', value: 'Friday' },
+      //   { name: 'Sáb', value: 'Saturday' }
+      // ];
+
+      var weekdays = [
+        { name: 'Dom', value: 0 },
+        { name: 'Seg', value: 1 },
+        { name: 'Ter', value: 2 },
+        { name: 'Qua', value: 3 },
+        { name: 'Qui', value: 4 },
+        { name: 'Sex', value: 5 },
+        { name: 'Sáb', value: 6 }
+      ];
+      
+      scope.weekdays = weekdays;
+      
+      var ordinals = [
+        { name: '1º', value: 'first' },
+        { name: '2º', value: 'second' },
+        { name: '3º', value: 'third' },
+        { name: '4º', value: 'forth' },
+        { name: 'Último', value: 'last' }
+      ];
+      
+      scope.ordinals = ordinals;
+      
+      
+      var deselectAllWeekdays = function(){
+        weekdays.forEach(function(weekday){
+          if (weekday) weekday.selected = false;
+        });
+      };
+      
+      var selectWeekdayByValue = function(value){
+        console.log('select weekday by value', value);
+        if (!value) return;
+        weekdays.forEach(function(weekday){
+          if (weekday && weekday.value === value) weekday.selected = true;
+        });
+      };
+      
+      var selectWeekdaysByValues = function(values){
+        console.log('select weekdays by values', values);
+        if (!values) return;
+        values.forEach(function(value){
+          selectWeekdayByValue(value);
+        });
+      };
+      
+      var deselectAllOrdinals = function(){
+        ordinals.forEach(function(ordinal){
+          if (ordinal) ordinal.selected = false;
+        });
+      };
+      
+      var selectOrdinalByValue = function(value){
+        console.log('select ordinal by value', value);
+        if (!value) return;
+        ordinals.forEach(function(weekday){
+          if (ordinal && ordinal.value === value) ordinal.selected = true;
+        });
+      };
+      
+      var selectOrdinalsByValues = function(values){
+        console.log('select weekdays by values', values);
+        if (!values) return;
+        values.forEach(function(value){
+          selectOrdinalByValue(value);
+        });
+      };
+      
+      scope.updateSelection = function(){
+        scope.selectedWeekdays = scope.weekdays.filter(function(weekday){
+          return weekday.selected;
+        }).map(function (weekday) {
+        	return weekday.value;
+        });
+        
+        scope.selection = scope.ordinals.filter(function(ordinal){
+          return ordinal.selected;
+        }).map(function (weekday) {
+        	return weekday.value;
+        });
+      };
+      
+      scope.$watch('selectedWeekdays', function(){
+        var selectedWeekdays = scope.selectedWeekdays;
+        deselectAllWeekdays();
+        if (selectedWeekdays) selectWeekdaysByValues(selectedWeekdays);
+      }, true);
+      
+      scope.$watch('selectedOrdinals', function(){
+        var selectedOrdinals = scope.selectedOrdinals;
+        deselectAllOrdinals();
+        if (selectedOrdinals) selectOrdinalsByValues(selectedOrdinals);
+      }, true);
+    }
+  };
 });
